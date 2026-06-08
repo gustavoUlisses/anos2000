c432db01ae1288710f8e24713e2675dcfd6ba8ee
gustavoUlisses <gustavoUlisses@users.noreply.github.com>
2026-06-08 00:38:04 -0300

    Fix react-xp fullscreen host

--- src/features/desktop/ReactXpExperience.tsx
+++ src/features/desktop/ReactXpExperience.tsx
@@ -5,8 +5,10 @@ import { Provider } from "@/features/desktop/react-xp/context/provider";
 
 export function ReactXpExperience() {
   return (
-    <Provider>
-      <ReactXpApp />
-    </Provider>
+    <div className="fixed inset-0 h-screen w-screen overflow-hidden bg-black">
+      <Provider>
+        <ReactXpApp />
+      </Provider>
+    </div>
   );
 }
