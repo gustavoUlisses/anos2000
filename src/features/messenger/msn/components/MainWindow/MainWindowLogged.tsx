c4e0c4333e226d5d583ec59a8cab709be9305853
gustavoUlisses <gustavoUlisses@users.noreply.github.com>
2026-06-08 19:09:15 -0300

    Remove logged MSN Live ID footer

--- src/features/messenger/msn/components/MainWindow/MainWindowLogged.tsx
+++ src/features/messenger/msn/components/MainWindow/MainWindowLogged.tsx
@@ -157,13 +157,6 @@ export function MainWindowLogged({
         <img src="https://gifdb.com/images/high/microsoft-internet-explorer-admk702irl7ymxag.webp" alt="Ads" className="w-75" />
       </div>
       <div className="msn-account-footer py-1 d-flex align-items-center justify-content-between w-100">
-        <div>
-          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z" />
-            <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
-          </svg>
-          <span className="text-secondary ps-1">Windows Live ID</span>
-        </div>
         <button className="logout-button" onClick={onLogout} type="button">
           Sair
         </button>
